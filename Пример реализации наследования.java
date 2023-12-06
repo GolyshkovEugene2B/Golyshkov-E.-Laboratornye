64b6a4a5546eacbbdf40d6ae0380a2e7da1e463c
Eugene <91383626+GolyshkovEvgenii@users.noreply.github.com>
2023-12-06 22:02:45 +0900

    Update Пример реализации наследования.java

--- Пример реализации наследования.java
+++ Пример реализации наследования.java
@@ -1,3 +1,5 @@
+//Если класс B наследует класс A, то все методы и поля класса A становятся доступными в классе B
+//Класс А
 public class A {
     private String name;
 
@@ -9,6 +11,8 @@ public class A {
         System.out.println(name);
     }
 }
+
+//Класс B, который наследует класс A
 public class B extends A {
     public B(String name) {
         super(name);
@@ -19,3 +23,10 @@ public class B extends A {
         System.out.println("New method from B class");
     }
 } 
+
+public static void main(String[] args) {
+B b = new B(“John”);
+b.printName();
+b.newMethod();
+}
+//Принцип наследования позволяет классам наследовать свойства и методы от других классов
