e58a895c59be1b3275d2036b98a6a4ae2e1305ec
Eugene <91383626+GolyshkovEvgenii@users.noreply.github.com>
2023-12-06 22:20:35 +0900

    Update Пример реализации инкапсуляции.java

--- Пример реализации инкапсуляции.java
+++ Пример реализации инкапсуляции.java
@@ -1,26 +1,13 @@
-class Car:
-    def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
+public class Person {
+   private String name;
+   private int age;
 
-    def set_speed(self, speed):
-        self.speed = speed
+   public void setName(String name) {
+      this.name = name;
+   }
 
-    def accelerate(self):
-        if self.speed < 100:
-            self.speed += 5
-        else:
-            print("Maximum speed reached")
-
-    def brake(self):
-        self.speed -= 5
-
-    @property
-    def get_speed(self):  # getter method for speed
-        return self.speed
-
-my_car = Car("BMW", "320i", 2018)
-my_car.accelerate()
-print(my_car.get_speed)
-my_car
+   public String getName() {
+      return name;
+   }
+}  
+//есть класс Person с двумя полями: name и age. Методы setName() и getName() доступны всем, а поля — только внутри класса Person.
